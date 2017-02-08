14a38d7d32bc0daafe132c414988cb7ad6782c0f
Rodrigo Q. Saramago <deepmarolaest@gmail.com>
2017-02-08 16:11:40 -0200

    Introspect facebook

--- token_introspection_endpoint.js
+++ token_introspection_endpoint.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('hapi')
 const Boom = require('boom')
+const Request = require('request')
 
 function encode(username, password) {
   return new Buffer(`${username}:${password}`).toString('base64')
@@ -20,7 +21,7 @@ function decodeAuthorizationHeader(header) {
 
 function buildToken(token) {
   var authToken = {}
-  //token = new Buffer(token, 'base64').toString('utf8')
+  token = new Buffer(token, 'base64').toString('utf8')
   switch (token) {
     case 'valid-grant-token':
       authToken = {
@@ -73,6 +74,32 @@ const validate = function (credentials) {
   return credentials.username == 'admin' && credentials.password == 'admin'
 }
 
+function fbauthorize(request, reply) {
+  const payload = request.payload
+  if (!payload) {
+    return reply(Boom.badRequest('Credentials must be provided'))
+  }
+
+  if (!payload.token) {
+    return reply(Boom.badRequest('Credentials must be provided'))
+  }
+
+  const url = `https://graph.facebook.com/me?access_token=${payload.token}`
+  Request(url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const result = JSON.parse(body)
+      const token = {
+        active: true,
+        scope: "public_profile user_friends",
+        client_id: result.id,
+        secret: 'blablabla'
+      }
+      return reply(token).type('application/json')
+    }
+    return reply(Boom.badRequest('Bad response from Facebook'))
+  })
+}
+
 const server = new Hapi.Server()
 server.connection({
   host: '0.0.0.0',
@@ -80,13 +107,20 @@ server.connection({
 })
 
 
-server.route({
-  method: 'POST',
-  path:'/token/introspect',
-  config: {
-    handler: function (request, reply) { authorize(request, reply) }
+server.route([{
+    method: 'POST',
+    path:'/token/introspect',
+    config: {
+      handler: function (request, reply) { authorize(request, reply) }
+    }
+  }, {
+    method: 'POST',
+    path: '/fb/token/introspect',
+    config: {
+      handler: function (request, reply) { fbauthorize(request, reply) }
+    }
   }
-})
+])
 
 
 server.start((err) => {
